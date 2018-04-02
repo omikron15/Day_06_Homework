39c322826394ac1db67e30ecdf0fc285ff0087ca
omikron15 <omikron15@hotmail.co.uk>
2018-04-02 20:45:23 +0100

    Stuck on Library class works

---
+++ library_class.rb
@@ -0,0 +1,22 @@
+class Library
+
+
+  attr_accessor :books_list
+  def initialize(books_list)
+    @books_list = books_list
+  end
+
+# def get_book_info_by_title(title_to_search)
+#
+#   for each_book in @books_list
+#
+#       # if each_book[:title] == title_to_search
+#       # #   #return each_book[:rental_details]
+#       # end #end of if
+#       return each_book[:title]
+#
+#   end #end of for
+#
+# end
+
+end #End of class
